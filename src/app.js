af2e6281f7d1220b6eb0b54a2210afcac79060e3
rupesh-dev30 <cgcrupesh@gmail.com>
2026-02-01 01:29:29 +0530

    chore: add ESLint and Prettier configuration
    - Updated package.json to include ESLint and Prettier scripts and dependencies.
    - Added .prettierrc for Prettier configuration.
    - Created .prettierignore to exclude specific files and directories.
    - Added eslint.config.js for ESLint configuration with custom rules.
    - Refactored import statements in app.js, index.js, and server.js to use single quotes.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).json("Hello from Acquisitions Service");
+app.get('/', (req, res) => {
+  res.status(200).json('Hello from Acquisitions Service');
 });
 
 export default app;
