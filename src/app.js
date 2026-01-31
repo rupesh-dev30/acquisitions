21f8d9c0fa028d259e88f25b3f5cc3d7ee3e5208
rupesh-dev30 <cgcrupesh@gmail.com>
2026-02-01 01:58:38 +0530

    feat: enhance project structure with logging and middleware integration

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import logger from '#config/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  morgan('combined', { stream: { write: msg => logger.info(msg.trim()) } })
+);
 
 app.get('/', (req, res) => {
-  res.status(200).json('Hello from Acquisitions Service');
+  logger.info('Hello from Acquisitions Service');
+  res.status(200).json('Hello from Acquisitions API Service');
 });
 
 export default app;
